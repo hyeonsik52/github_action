9446936dee4de01990d84eca8e5da6d87e4b09f5
nexmondTW <nexmond@twinny.co.kr>
2021-08-10 18:05:58 +0900

    Add colors

--- TARAS/Utilities/Colors.swift
+++ TARAS/Utilities/Colors.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 extension UIColor {
-    static let purple4A3C9F = UIColor(hex: "4A3C9F")
     static let lightPurpleEBEAF4 = UIColor(hex: "EBEAF4")
     static let higlightPurpleF1EFFF = UIColor(hex: "F1EFFF")
     static let redEB4D39 = UIColor(hex: "EB4D39")
     static let lightRedFFE8E6 = UIColor(hex: "FFE8E6")
     static let black0F0F0F = UIColor(hex: "0F0F0F")
-    static let black303030 = UIColor(hex: "303030")
     static let gray9A9A9A = UIColor(hex: "9A9A9A")
     static let lightGrayF6F6F6 = UIColor(hex: "F6F6F6")
     static let grayE6E6E6 = UIColor(hex: "E6E6E6")
@@ -24,8 +22,6 @@ extension UIColor {
     static let grayA2A2A2 = UIColor(hex: "A2A2A2")
     static let gray999999 = UIColor(hex: "999999")
     static let grayCDCDCD = UIColor(hex: "CDCDCD")
-    static let gray787878 = UIColor(hex: "787878")
-    static let grayC9C9C9 = UIColor(hex: "C9C9C9")
     static let lightGrayF2F2F2 = UIColor(hex: "F2F2F2")
     static let lightGrayF0F0F0 = UIColor(hex: "F0F0F0")
     static let skyBlue85AEFF = UIColor(hex: "85AEFF")
@@ -35,4 +31,91 @@ extension UIColor {
     static let grayA5A5A5 = UIColor(hex: "A5A5A5")
     static let networkGreen6DD400 = UIColor(hex: "6DD400")
     static let networkRedFE4242 = UIColor(hex: "FE4242")
+
+    
+    // Primary Color
+    
+    // Primary
+    static let purple351E7E = UIColor(hex: "351E7E")
+    static let purple423293 = UIColor(hex: "423293")
+    static let purple4A3C9F = UIColor(hex: "4A3C9F")
+    static let purple5246AB = UIColor(hex: "5246AB")
+    static let purple584FB5 = UIColor(hex: "584FB5")
+    static let purple6E6AC1 = UIColor(hex: "6E6AC1")
+    static let purple8685CC = UIColor(hex: "8685CC")
+    static let purpleA7A8DB = UIColor(hex: "A7A8DB")
+    static let purpleCACAEA = UIColor(hex: "CACAEA")
+    static let purpleEAEAF6 = UIColor(hex: "EAEAF6")
+    
+    // Secondary
+    static let redB64328 = UIColor(hex: "B64328")
+    static let redD04E2E = UIColor(hex: "D04E2E")
+    static let redDF5432 = UIColor(hex: "DF5432")
+    static let redED5B37 = UIColor(hex: "ED5B37")
+    static let redF8613B = UIColor(hex: "F8613B")
+    static let redF87755 = UIColor(hex: "F87755")
+    static let redF88F73 = UIColor(hex: "F88F73")
+    static let redF9AE9B = UIColor(hex: "F9AE9B")
+    static let redFBCEC2 = UIColor(hex: "FBCEC2")
+    static let redF9EAE9 = UIColor(hex: "F9EAE9")
+    
+    // Tint color & Shade color
+    // Purple
+    static let lightPurpleEDECF5 = UIColor(hex: "EDECF5")
+    static let lightPurpleD1CEE6 = UIColor(hex: "D1CEE6")
+    static let purpleB7B1D9 = UIColor(hex: "B7B1D9")
+    static let purple9B93CA = UIColor(hex: "9B93CA")
+    static let purple8077BC = UIColor(hex: "8077BC")
+    static let purple6459AD = UIColor(hex: "6459AD")
+    //-> purple4A3C9F <-
+    static let purple3D3387 = UIColor(hex: "3D3387")
+    static let purple342A6F = UIColor(hex: "342A6F")
+    static let purple282057 = UIColor(hex: "282057")
+    static let darkPurple1E1840 = UIColor(hex: "1E1840")
+    static let darkPurple120F27 = UIColor(hex: "120F27")
+    // Red
+    static let lightRedFEEFEB = UIColor(hex: "FEEFEB")
+    static let lightRedFDD7CD = UIColor(hex: "FDD7CD")
+    static let redFCC0B1 = UIColor(hex: "FCC0B1")
+    static let redFBA893 = UIColor(hex: "FBA893")
+    static let redFA9076 = UIColor(hex: "FA9076")
+    static let redF97859 = UIColor(hex: "F97859")
+    //-> redF8613B <-
+    static let redD35231 = UIColor(hex: "D35231")
+    static let redAE4429 = UIColor(hex: "AE4429")
+    static let red87351F = UIColor(hex: "87351F")
+    static let darkRed632718 = UIColor(hex: "632718")
+    static let darkRed3F180E = UIColor(hex: "3F180E")
+    
+    // Gray scale
+    static let lightGrayF1F1F1 = UIColor(hex: "F1F1F1")
+    static let lightGrayDDDDDD = UIColor(hex: "DDDDDD")
+    static let grayC9C9C9 = UIColor(hex: "C9C9C9")
+    static let grayB4B4B4 = UIColor(hex: "B4B4B4")
+    static let grayA0A0A0 = UIColor(hex: "A0A0A0")
+    static let gray8C8C8C = UIColor(hex: "8C8C8C")
+    static let gray787878 = UIColor(hex: "787878")
+    static let gray666666 = UIColor(hex: "666666")
+    static let gray535353 = UIColor(hex: "535353")
+    static let gray414141 = UIColor(hex: "414141")
+    static let darkGray303030 = UIColor(hex: "303030")
+    static let darkGray1E1E1E = UIColor(hex: "1E1E1E")
+    
+    
+    // Status Color
+    
+    // Negative, Error Cancel
+    static let redF80003 = UIColor(hex: "F80003")
+    
+    // Warning, Caution
+    static let yellowFF9213 = UIColor(hex: "FF9213")
+    
+    // Positive, Success
+    static let green00BB23 = UIColor(hex: "00BB23")
+    
+    // Safery, Help, Information
+    static let blue0C68F8 = UIColor(hex: "0C68F8")
+    
+    // Neutral
+    //-> gray8C8C8C <-
 }
