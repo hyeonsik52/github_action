a3709c0a03419c82162efb5f4ee000abef5e3e48
nexmondTW <nexmond@twinny.co.kr>
2021-10-22 16:23:21 +0900

    Add config logout to REST API

--- TARAS/Managers/NetworkManager/REST/RestAPIType.swift
+++ TARAS/Managers/NetworkManager/REST/RestAPIType.swift
@@ -10,12 +10,15 @@ import Foundation
 enum RestAPIType<T: RestAPIResponse> {
     
     case login(input: LoginRequestModel)
+    case logout(input: LogoutRequestModel)
     
     var url: URL {
         let path: String = {
             switch self {
             case .login:
                 return "auth/token/"
+            case .logout:
+                return "auth/revoke_token/"
             }
         }()
         return URL(string: "http://graass-dev.twinny-tarp.com:8000/")!.appendingPathComponent(path)
@@ -24,6 +27,8 @@ enum RestAPIType<T: RestAPIResponse> {
     var parameters: [String: Any] {
         switch self {
         case .login(let input):
+            return input.dictionary
+        case .logout(let input):
             return input.dictionary
         }
     }
