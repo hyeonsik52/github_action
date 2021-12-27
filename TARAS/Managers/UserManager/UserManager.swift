40543d2e2b32f5a7c07b09803df17ad7af4a81df
nexmondTW <nexmond@twinny.co.kr>
2021-12-27 13:00:19 +0900

    Delete comment

--- TARAS/Managers/UserManager/UserManager.swift
+++ TARAS/Managers/UserManager/UserManager.swift
@@ -62,58 +62,7 @@ extension UserManager {
 
     /// access token 갱신
     func reAuthenticate(_ accessToken: String, _ completion: @escaping(Error?) -> Void) {
-    
-//        guard let clientInfo = self.userTB.clientInfo,
-//              let refreshToken = self.userTB.refreshToken else { return }
-//
-//        let input = RefreshSessionInput(
-//            clientInfo: clientInfo,
-//            clientType: "ios",
-//            refreshToken: refreshToken
-//        )
-//
-//        // 2. 서버의 토큰이 만료된 상태(하지만 클라에서는 호출이 이루어지지 않아 만료 상태를 모르는 상태)에서 2개 이상의
-//        // 호출이 일어날 경우 동시에 재인증 과정이 진행될 수 있기 때문에,
-//        // isReAuthenticating 플래그로 인증 중에 진입하는 경우 재시도하도록 한다.
-//        guard !self.isReAuthenticating,
-//            // 3. 재인증 완료된 후, 이전의 호출이 이전 토큰을 가지고 시도할 수 있기 때문에,
-//            // 호출의 토큰과 현재 토큰이 같은 때만 통과시킨다
-//            accessToken == self.provider.userManager.userTB.accessToken else {
-//                completion(nil)
-//                return
-//        }
-//
-//        self.isReAuthenticating = true
-//
-//        self.provider.networkManager
-//            .perform(RefreshSessionMutation(input: input))
-//            .map { $0.refreshSessionMutation }
-//            .subscribe(onNext: { [weak self] data in
-//                guard let self = self else { return }
-//
-//                if let error = data.asRefreshSessionError {
-//                    if error.errorCode == .invalidRefreshToken {
-//                        let error = NSError(domain: "", code: 401, userInfo: nil)
-//                        completion(error)
-//                    }
-//                }
-//
-//                if let payload = data.asLoginPayload {
-//                    self.updateTokens(access: payload.accessToken, refresh: payload.refreshToken)
-//                    self.provider.networkManager.updateWebSocketTransportConnectingPayload()
-//
-//                    // 1. completion(nil)은 재호출 하는 기능이기 때문에, 성공한 경우에만 사용해야 함.
-//                    completion(nil)
-//                }
-//                self.isReAuthenticating = false
-//            }, onError: { [weak self] error in
-//                completion(error)
-//                self?.isReAuthenticating = false
-//            }).disposed(by: self.disposeBag)
-    
-        //TODO: 재로그인 처리?
         //현재는 갱신 방법이 없어서 무조건 실패
-        
         let error = NSError(domain: "", code: 401, userInfo: nil)
         completion(error)
     }
