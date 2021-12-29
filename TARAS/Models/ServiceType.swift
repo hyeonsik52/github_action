687c32b078a58ee8d4ad7c3c8d5eafbac890676c
nexmondTW <nexmond@twinny.co.kr>
2021-12-29 19:15:34 +0900

    Modify service types

--- TARAS/Models/ServiceType.swift
+++ TARAS/Models/ServiceType.swift
@@ -8,64 +8,10 @@
 import Foundation
 
 ///서비스 타입
-enum ServiceType: Int {
+enum ServiceType: String {
     
-    ///카페 배송
-    case cafe
-    ///택배 배송
-    case parcel
-    ///일반 배송
-    case general
-    
-    init(type: String) {
-        switch type {
-        case "CAFE":
-            self = .cafe
-        case "GENERAL":
-            self = .general
-        default:
-            self = .parcel
-        }
-    }
-    
-    static let all: [Self] = [.cafe, .parcel, .general]
+    ///일반 (N)
+    case general = "GENERAL"
+    ///로딩-언로딩 (L)
+    case loading = "LOADING"
 }
-
-extension ServiceType {
-    
-    var description: String {
-        switch self {
-        case .cafe:
-            return "카페"
-        case .parcel:
-            return "택배"
-        case .general:
-            return "일반"
-        }
-    }
-
-    var requestMenu: String {
-        return "\(self.description) 배송"
-    }
-
-    var listMenu: String {
-        switch self {
-        case .general:
-            return self.requestMenu
-        default:
-            return self.description
-        }
-    }
-    
-    var toString: String {
-        switch self {
-        case .cafe:
-            return "CAFE"
-        case .parcel:
-            return "PARCEL"
-        case .general:
-            return "GENERAL"
-        }
-    }
-}
-
