eb3aa87e139995d9b14afa9cf6612c95c6c08a2a
nexmondTW <nexmond@twinny.co.kr>
2022-02-04 12:32:16 +0900

    Improve service state type

--- TARAS/Models/ServiceState.swift
+++ TARAS/Models/ServiceState.swift
@@ -8,35 +8,39 @@
 import Foundation
 
 /// 서비스 상태
-enum ServiceState: String {
+enum ServiceState {
     /// 로봇 배정 중
-    case robotAssigning = "Initialization"
+    case robotAssigning
     /// 이동 대기 중
-    case waiting = "waiting_for_robot_to_depart"
+    case waiting
     /// 이동 중
-    case moving = "waiting_for_robot_to_arrive"
+    case moving
     /// 정차지 도착 (작업 대기)
-    case arrived = "waiting_for_work_to_complete"
+    case arrived
     /// 서비스 완료
-    case finished = "Finished"
+    case finished
     /// 서비스 중단
-    case canceled = "Canceled"
+    case canceled
     /// 서비스 취소
-    case failed = "Failed"
+    case failed
     /// 복귀 중
-    case returning = "Returning"
+    case returning
     /// 알 수 없는 상태
-    case unknown = ""
+    case unknown
     
     init(state: String) {
         switch state {
-        case "Initialization":
+        case "Initialized",
+            "waiting_for_robot_to_be_assigned_to_mission":
             self = .robotAssigning
         case "waiting_for_robot_to_depart":
             self = .waiting
         case "waiting_for_robot_to_arrive":
             self = .moving
-        case "waiting_for_work_to_complete":
+        case "waiting_for_work_to_complete",
+            "waiting_for_verification_code",
+            "waiting_for_opening_parcel_lift",
+            "waiting_for_closing_parcel_lift":
             self = .arrived
         case "Finished":
             self = .finished
@@ -48,6 +52,37 @@ enum ServiceState: String {
             self = .returning
         default:
             self = .unknown
+        }
+    }
+    
+    var rawValues: [String] {
+        switch self {
+        case .robotAssigning:
+            return [
+                "Initialized",
+                "waiting_for_robot_to_be_assigned_to_mission"
+            ]
+        case .waiting:
+            return ["waiting_for_robot_to_depart"]
+        case .moving:
+            return ["waiting_for_robot_to_arrive"]
+        case .arrived:
+            return [
+                "waiting_for_work_to_complete",
+                "waiting_for_verification_code",
+                "waiting_for_opening_parcel_lift",
+                "waiting_for_closing_parcel_lift"
+            ]
+        case .finished:
+            return ["Finished"]
+        case .canceled:
+            return ["Canceled"]
+        case .failed:
+            return ["Failed"]
+        case .returning:
+            return ["Returning"]
+        default:
+            return []
         }
     }
 }
