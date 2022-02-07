5aec72e94e8487e9173a6bb05232393911aca9a6
nexmondTW <nexmond@twinny.co.kr>
2022-02-07 19:07:02 +0900

    Modify ServiceDetailViewReactor
    - Action 명 수정
    - Mutation 네트워크 통신 수정
    - Mutation 오류 처리
    - State 항목 추가

--- TARAS/Presentations/Workspace/WorkspaceTabBar/ServiceManagement/ServiceDetailViewController/ServiceDetailViewReactor.swift
+++ TARAS/Presentations/Workspace/WorkspaceTabBar/ServiceManagement/ServiceDetailViewController/ServiceDetailViewReactor.swift
@@ -11,153 +11,152 @@ import ReactorKit
 
 class ServiceDetailViewReactor: Reactor {
     
+    enum Text {
+        static let errorRequestFailed = "서비스를 취소하지 못했습니다."
+        static let errorNetworkConnection = "서버와의 통신이 원활하지 않습니다."
+    }
+    
     let scheduler: Scheduler = SerialDispatchQueueScheduler(qos: .userInitiated)
     let disposeBag = DisposeBag()
     
     enum Action {
-        case loadService(refresh: Bool)
-        case cancelRequest
-        case deleteService
-        case judgeIsFromPush
+        case refreshService
+        case cancelService
     }
     
     enum Mutation {
-        case loadedService(Service)
-        case requestCanceled(Bool)
-        case servceDeleted(Bool)
-        case isLoading(Bool?) //초기 상태 nil, 로딩 시작 true, 로딩 종료 false/ nil에서 true가 된 경우일 때만 스켈레톤 뷰 출력
-        case isProcessing(Bool)
-        case updateIsFromPush(Bool)
+        case refreshService(Service)
+        case updateIsCanceled(Bool)
+        case updateIsLoading(Bool?)
+        case updateIsProcessing(Bool?)
+        case updateErrorMessage(String?)
     }
     
     struct State {
-        var serviceModel: Service?
-        var requestCanceled: Bool?
-        var serviceDeleted: Bool?
+        var service: Service?
+        var isCanceled: Bool?
         var isLoading: Bool?
         var isProcessing: Bool?
-        var isFromPush: Bool
+        var errorMessage: String?
     }
     
-    var initialState: State {
-        return State(
-            serviceModel: nil,
-            requestCanceled: nil,
-            serviceDeleted: nil,
-            isLoading: nil,
-            isProcessing: nil,
-            isFromPush: false
-        )
-    }
+    let initialState: State = .init(
+        service: nil,
+        isCanceled: nil,
+        isLoading: nil,
+        isProcessing: nil,
+        errorMessage: nil
+    )
     
     let provider : ManagerProviderType
     let workspaceId: String
     let serviceId: String
-    let pushInfo: NotificationInfo?
     
     init(
         provider: ManagerProviderType,
         workspaceId: String,
-        serviceId: String,
-        pushInfo: NotificationInfo? = nil
+        serviceId: String
     ) {
         self.provider = provider
         self.workspaceId = workspaceId
         self.serviceId = serviceId
-        self.pushInfo = pushInfo
-        
+    }
+    
+    private func bind() {
         //temp
 //        self.provider.subscriptionManager.service(by: self.serviceId)
-//             .subscribe(onNext: { [weak self] result in
-//                 self?.action.onNext(.loadService(refresh: true))
-//             })
-//             .disposed(by: self.disposeBag)
+//            .subscribe(onNext: { [weak self] result in
+//                self?.action.onNext(.refreshService)
+//            }).disposed(by: self.disposeBag)
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        case .loadService(let refresh):
-            //temp
-//            let query = ServiceQuery(serviceId: self.serviceId)
-//            let request = (refresh ? self.provider.networkManager.fetch(query): self.provider.networkManager.fetch(query))
+        case .refreshService:
+            let query = ServiceQuery(workspaceId: self.workspaceId, serviceId: self.serviceId)
             return .concat([
-                .just(.isLoading(true)),
+                .just(.updateIsLoading(true)),
                 
-                //temp
-//                request
-//                    .compactMap { $0.hiGlovisServiceByOrderId?.fragments.serviceFragment }
-//                    .compactMap { self.provider.serviceManager.convert(service: $0) }
-//                    .map { Mutation.loadedService($0) },
+                self.provider.networkManager.fetch(query)
+                    .compactMap { $0.signedUser?.joinedWorkspaces?.edges.first??.node }
+                    .compactMap { $0.services?.edges.first??.node?.fragments.serviceFragment }
+                    .map { .refreshService(.init($0)) }
+                    .catch(self.catchClosure),
                 
-                .just(.isLoading(false))
+                .just(.updateIsLoading(false))
             ])
-        case .cancelRequest:
-//            let input = CancelServiceInput(serviceIdx: self.serviceIdx)
+        case .cancelService:
+            let mutation = CancelServiceMutation(id: self.serviceId)
             return .concat([
-                .just(.isProcessing(true)),
+                .just(.updateIsProcessing(true)),
                 
-//                self.provider.networkManager.perform(CancelServiceMutationMutation(input: input))
-//                .map { $0.cancelServiceMutation.asCancelServicePayload?.result == "1" }
-//                .map { Mutation.requestCanceled($0) },
+                self.provider.networkManager.perform(mutation)
+                    .flatMapLatest { payload -> Observable<Mutation> in
+                        let isCanceled = payload.cancelService?.ok == true
+                        return .just(.updateIsCanceled(isCanceled))
+                    }.catch(self.catchClosure),
                 
-                .just(.isProcessing(false))
+                .just(.updateIsProcessing(false))
             ])
-        case .deleteService:
-//            let input = HideServiceInput(serviceIdx: self.serviceIdx)
-            return .concat([
-                .just(.isProcessing(true)),
-                
-//                self.provider.networkManager.perform(HideServiceMutationMutation(input: input))
-//                .map { $0.hideServiceMutation.asHideServicePayload?.result == "1" }
-//                .map { Mutation.servceDeleted($0) },
-                
-                .just(.isProcessing(false))
-            ])
-            
-        case .judgeIsFromPush:
-            let isFromPush = (self.pushInfo != nil)
-            return .just(.updateIsFromPush(isFromPush))
+        }
+    }
+    
+    var catchClosure: ((Error) throws -> Observable<Mutation>) {
+        return { error in
+            guard let multipleError = error as? MultipleError,
+                  let errors = multipleError.graphQLErrors else {
+                return .just(.updateErrorMessage(Text.errorNetworkConnection))
+            }
+            for error in errors {
+                guard let message = error.message, !message.isEmpty else { continue }
+                return .just(.updateErrorMessage(message))
+            }
+            return .just(.updateErrorMessage(Text.errorRequestFailed))
         }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
         var state = state
         switch mutation {
-        case let .loadedService(serviceModel):
-            state.serviceModel = serviceModel
-            state.isFromPush = false
-            
-        case let .requestCanceled(successed):
-            state.requestCanceled = successed
-            state.isFromPush = false
-            
-        case let .servceDeleted(successed):
-            state.serviceDeleted = successed
-            state.isFromPush = false
-            
-        case .isLoading(let loading):
-            state.isLoading = loading
-            state.isFromPush = false
-            
-        case .isProcessing(let processing):
-            state.isProcessing = processing
-            state.isFromPush = false
-            
-        case let .updateIsFromPush(isFromPush):
-            state.isFromPush = isFromPush
+        case .refreshService(let model):
+            state.service = model
+        case .updateIsCanceled(let isCanceled):
+            state.isCanceled = isCanceled
+        case .updateIsLoading(let isLoading):
+            state.isLoading = isLoading
+        case .updateIsProcessing(let isProcessing):
+            state.isProcessing = isProcessing
+        case .updateErrorMessage(let message):
+            state.errorMessage = message
         }
         return state
     }
+}
+
+extension ServiceDetailViewReactor {
     
     func reactorForBasicInfo() -> ServiceBasicInfoViewReactor {
-        return ServiceBasicInfoViewReactor(provider: self.provider, workspaceId: self.workspaceId, serviceId: self.serviceId)
+        return .init(
+            provider: self.provider,
+            workspaceId: self.workspaceId,
+            serviceId: self.serviceId
+        )
     }
     
     func reactorForDetailLog() -> ServiceDetailLogViewReactor {
-        return ServiceDetailLogViewReactor(provider: self.provider, workspaceId: self.workspaceId, serviceId: self.serviceId)
+        return .init(
+            provider: self.provider,
+            workspaceId: self.workspaceId,
+            serviceId: self.serviceId
+        )
     }
     
     func reactorForWorkRequest(with serviceUnitId: String) -> WorkRequestViewReactor {
-        return WorkRequestViewReactor(provider: self.provider, workspaceId: self.workspaceId, serviceId: self.serviceId, serviceUnitId: serviceUnitId)
+        return .init(
+            provider: self.provider,
+            workspaceId: self.workspaceId,
+            serviceId: self.serviceId,
+            serviceUnitId: serviceUnitId
+        )
     }
 }
