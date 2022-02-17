4c033ca64df50861007bfec7e88adcd35b9fc9a0
nexmondTW <nexmond@twinny.co.kr>
2022-02-17 17:54:30 +0900

    Modify model NorificationInfo

--- TARAS/Managers/PushManager/Model/NotificationInfo.swift
+++ TARAS/Managers/PushManager/Model/NotificationInfo.swift
@@ -9,27 +9,12 @@ import Foundation
 
 struct NotificationInfo {
     
-    var notificationType: NotificationType
-    var workspaceId: String
+    var notificationType: NotificationType = .received
+    var workspaceId: String?
     var serviceId: String?
-    var serviceUnitId: String?
-    var stopId: String?
     
-    init?(_ info: [String: AnyHashable]) {
-        guard let type = info["notification_type"] as? String,
-              let workspaceId = info["workspace_id"] as? String else { return nil }
-        
-        self.notificationType = NotificationType.init(rawValue: type) ?? .default
-        self.workspaceId = workspaceId
-        
-        if let serviceId = info["service_id"] as? String {
-            self.serviceId = serviceId
-        }
-        if let serviceUnitId = info["service_unit_id"] as? String {
-            self.serviceUnitId = serviceUnitId
-        }
-        if let stopId = info["stop_id"] as? String {
-            self.stopId = stopId
-        }
+    init(_ info: [String: Any]) {
+        self.workspaceId = info["workspaceId"] as? String
+        self.serviceId = info["serviceId"] as? String
     }
 }
